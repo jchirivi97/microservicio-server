78a603d575e29e5076fb863c944cdd3f995bb0d1
jimmy chirivi <jimmychirivi09@gmail.com>
2020-05-09 21:59:00 -0500

    app ok

---
+++ src/main/java/edu/escuelaing/arep/proyecto/ProyectoApplication.java
@@ -0,0 +1,19 @@
+package edu.escuelaing.arep.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ProyectoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoApplication.class, args);
+	}
+
+}
